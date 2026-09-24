765e462a69e22f5e8cb68572a7792d44aec94264
Sasidu Wshshanka <sasiduwishshanka2005@gmail.com>
2026-09-24 19:05:25 +0530

    Add files via upload

---
+++ MQ-2 GAS Leak Detecter/code/main.ino
@@ -0,0 +1,57 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define A_PIN 34
+#define D_PIN 15
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Wire.begin(21, 22);
+
+  pinMode(A_PIN,INPUT);
+  pinMode(D_PIN,INPUT); 
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("MQ-2 Starting..");
+
+  delay(1000);
+
+  lcd.clear();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int sensorState = digitalRead(D_PIN);
+  int sensorValue  = analogRead(A_PIN);
+
+  if(sensorState == LOW){
+    lcd.setCursor(0, 0);
+    lcd.print("GAS LEAK");
+
+    lcd.setCursor(0, 1);
+    lcd.print("G-VALUE: ");
+    lcd.print(sensorValue);
+  }
+  else{
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("NO LEAK");
+
+    lcd.setCursor(0, 1);
+    lcd.print("G-VALUE: ");
+    lcd.print(sensorValue);
+
+  }
+
+  delay(500);
+
+}
